2d724b59b9e4bfbd64d3c3e7218b0603e1462497
Xinran Zhao <1259449968@qq.com>
2024-04-29 11:35:48 +0800

    修改作品总览

---
+++ src/components/Exhibition/allWorks/contentCard.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="card">
+    <img :src="imageUrl" alt="Card image" />
+    <div class="card-body">
+      <h5 class="card-title">{{ title }}</h5>
+      <p class="card-text">{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "card",
+  props: {
+    imageUrl: String,
+    title: String,
+    description: String,
+  },
+};
+</script>
+
+<style scoped>
+.card {
+  width: 300px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin: 10px;
+  overflow: hidden;
+}
+
+.card img {
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+}
+
+.card-body {
+  padding: 10px;
+}
+
+.card-title {
+  font-size: 18px;
+  margin-bottom: 10px;
+}
+
+.card-text {
+  font-size: 14px;
+}
+</style>
