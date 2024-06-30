e45deb9532b8b9a897070d68b6acfb02544bb6af
qwejk <qwejkl2222ccyy@126.com>
2024-07-01 05:24:08 +0800

    添加iframe框架

--- src/pages/DigitalArtwork/DigitalArtwork.vue
+++ src/pages/DigitalArtwork/DigitalArtwork.vue
@@ -138,7 +138,7 @@ export default {
             const body = res.data
             console.log(res)
             if(body.msg === "SUCCESS") {
-              this.$cookies.set('token', body.data.token.split(' ')[1])
+              this.$cookies.set('token', body.data.token.split(' ')[1], 0, '/', 'localhost', false, 'Lax');
               this.loaded = true;
             }else {
               const userRegister = {
@@ -153,7 +153,7 @@ export default {
                   const body = res.data
                   console.log(res)
                   if(body.msg === "SUCCESS") {
-                    this.$cookies.set('token', body.data.token.split(' ')[1])
+                    this.$cookies.set('token', body.data.token.split(' ')[1], 0, '/', 'localhost', false, 'Lax');
                     this.loaded = true;
                   }
                 })
